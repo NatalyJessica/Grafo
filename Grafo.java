d5b6a326234b24e8fe5f9dfc875f3284534c7197
Nataly Jéssica Ribeiro de Souza <106627940+NatalyJessica@users.noreply.github.com>
2024-09-03 14:03:19 -0300

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -14,6 +14,23 @@ public class Grafo<X> {
         vertices.guardeUmItemNoInicio(novo);
     }
 
+    public Vertice<X> buscarVertesse(X info){
+        //verificar se tem informação
+        if(info == null)
+            System.out.printl("Informação ausente");
+        No<Vertice<X>> atual = this.vertices.getPrimeiro();
+        while(atual != null){
+          Vertice<X> v = atual.getInfo();
+            if(v.getinfo().equals(info)){
+                return v;
+            }
+
+            atual= atual.getProx();
+        }
+
+        return null;
+        
+    }
 
     @Override
     public String toString() {
